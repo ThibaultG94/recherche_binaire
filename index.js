84769f05a48b932e93ed889fa01eb8624af90afd
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-23 15:25:32 +0200

    recursive

--- index.js
+++ index.js
@@ -10,19 +10,36 @@ function randomArray() {
   }
 }
 
-randomArray();
-myArray.sort((a, b) => a - b);
-console.log(myArray);
-
 const findElement = (array, thingToFind) => {
   for (let element of array) {
     if (element === thingToFind) {
-      console.log("true");
       return true;
     }
   }
-  console.log("false");
   return false;
 };
 
+const binarySearch = (array, thingToFind, start, end) => {
+  if (start > end) {
+    console.log("false");
+    return false;
+  }
+  let mid = Math.floor((start + end) / 2);
+  if (array[mid] === thingToFind) {
+    console.log("true");
+    return true;
+  }
+
+  if (thingToFind < array[mid]) {
+    return binarySearch(array, thingToFind, start, mid - 1);
+  } else {
+    return binarySearch(array, thingToFind, mid + 1, end);
+  }
+  console.log(start, mid, end);
+};
+
+randomArray();
+myArray.sort((a, b) => a - b);
+console.log(myArray);
 findElement(myArray, 20);
+binarySearch(myArray, 20, 0, myArray.length);
