0cbf33857e809befb622631ff2b109bfe5afeb0a
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-23 15:00:13 +0200

    array + function

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const findElement = (array, thingtoFind) => {
+let myArray = [];
+let i = 0;
+
+function randomArray() {
+  if (i < 100) {
+    let randomNumber = Math.floor(Math.random() * `${i}0`);
+    myArray.push(randomNumber);
+    i++;
+    randomArray();
+  }
+}
+
+randomArray();
+myArray.sort((a, b) => a - b);
+console.log(myArray);
+
+const findElement = (array, thingToFind) => {
   for (let element of array) {
     if (element === thingToFind) {
+      console.log("true");
       return true;
     }
   }
+  console.log("false");
   return false;
 };
+
+findElement(myArray, 20);
